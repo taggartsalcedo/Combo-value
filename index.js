eb055c7a4de74520013e1f180951a279d28d5500
Taggart <t.salcedo@hotmail.com>
2023-10-23 15:56:04 -0400

    files saved to local machine, commit and upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//Overall goal is to create an alert with the end code 10-40-39
+//Set 1st value
+const val1 = 8 + 2;
+//Set 2nd value
+const val2 = 45 - 5;
+//set 3rd value
+const val3 = 3 * 13;
+
+//Test set values
+console.log(val2)
+
+//create string to give value to users in alert
+
+const valTotal= (`${val1} - ${val2} - ${val3}`);
+
+alert(valTotal);
